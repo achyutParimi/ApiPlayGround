cce28a3335a98518c9dae5400adb09572ffeb9fc
dspy-dev <33550883+dspy-dev@users.noreply.github.com>
2023-08-05 14:02:56 -0700

    API Playground for fetching realEstateData using rapidApi-initial commit

---
+++ src/main/java/com/apiplayground/apirepository/ApiRepository.java
@@ -0,0 +1,2 @@
+package com.apiplayground.apirepository;public class ApiRepository {
+}
---
+++ src/test/java/com/apiplayground/ApiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
